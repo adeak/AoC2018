b8fe4b2b61bb6501078daacbe9081e8f46728614
adeak <adeak@users.noreply.github.com>
2018-12-09 11:50:52 +0100

    Day 9 fast

--- day09.py
+++ day09.py
@@ -1,58 +1,39 @@
 from itertools import cycle
+from collections import deque
 
-class Marble():
-    def __init__(self, c):
-        self.val = c
-        self.prev = None
-        self.next = None
+class Marbles(deque):
+    def moveclock(self, n=1):
+        self.rotate(-n)
     
-    def getclock(self, n=1):
-        current = self
-        for _ in range(n):
-            current = current.next
-        return current
-    
-    def getcounter(self, n=1):
-        current = self
-        for _ in range(n):
-            current = current.prev
-        return current
+    def movecounter(self, n=1):
+        self.rotate(n)
     
     def insertright(self, val):
-        new = Marble(val)
-        new.prev = self
-        new.next = self.next
-        self.next.prev = new
-        self.next = new
-        return new
+        self.moveclock()
+        self.appendleft(val)
 
     def remove(self):
-        self.prev.next = self.next
-        self.next.prev = self.prev
+        self.popleft()
 
 def day09(inp, factor=1):
     vals = inp.split()
     nplay = int(vals[0])
     nmarb = int(vals[-2])*factor + 1
-    current = Marble(0)
-    current.next = current.prev = current
+    marbles = Marbles([0])
     scores = [0]*nplay
     elfit = iter(cycle(range(nplay)))
     for val in range(1, nmarb):
         elf = next(elfit)
         if not val % 23:
-            to_del = current.getcounter(7)
-            scores[elf] += val + to_del.val
-            current = to_del.getclock()
-            to_del.remove()
+            marbles.movecounter(7)
+            scores[elf] += val + marbles[0]
+            marbles.remove()
         else:
-            current = current.getclock().insertright(val)
+            marbles.moveclock()
+            marbles.insertright(val)
 
     return max(scores)
 
-
-def day09b(inp):
-    pass
 
 if __name__ == "__main__":
     testinp = open('day09.testinp').read().strip()
