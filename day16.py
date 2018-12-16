fd162101bb03036188a9c59b27d9e0cbeaa13a78
adeak <adeak@users.noreply.github.com>
2018-12-16 20:17:53 +0100

    Day 16 "simplified"

--- day16.py
+++ day16.py
@@ -1,116 +1,52 @@
 from ast import literal_eval
+from operator import add, mul, and_, or_, gt, eq
 
-instrs = {}
+def binary_maker():
+    """Generates instructions that correspond to binary operations"""
+    instrs = {}
+    # choose operation type
+    ops = {'add': add, 'mul': mul, 'ban': and_, 'bor': or_}
+    for name,op in ops.items():
+        for tail in 'ri':
+            def fun(A, B, C, regs, op=op, tail=tail):
+                out = regs.copy()
+                out[C] = op(regs[A], B if tail == 'i' else regs[B])
+                return out
+            instrs[name + tail] = fun
+    return instrs
 
-def store(fun):
-    #def wrapped(A, B, C, regs):
-    #    out = fun(A, B, C, regs)
-    #    #print(f'{fun.__name__}: {regs} -> {out}')
-    #    return out
-    #instrs[fun.__name__] = wrapped
-    instrs[fun.__name__] = fun
-    return fun
+def asgn_maker():
+    """Generates instructions that correspond to assignments"""
+    instrs = {}
+    prefix = 'set'
+    for tail in 'ri':
+        def fun(A, B, C, regs, tail=tail):
+            out = regs.copy()
+            out[C] = A if tail == 'i' else regs[A]
+            return out
+        instrs[prefix + tail] = fun
+    return instrs
 
-@store
-def addr(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] + regs[B]
-    return out
-
-@store
-def addi(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] + B
-    return out
-
-@store
-def mulr(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] * regs[B]
-    return out
-
-@store
-def muli(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] * B
-    return out
-
-@store
-def banr(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] & regs[B]
-    return out
-
-@store
-def bani(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] & B
-    return out
-
-@store
-def borr(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] | regs[B]
-    return out
-
-@store
-def bori(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A] | B
-    return out
-
-@store
-def setr(A, B, C, regs):
-    out = regs.copy()
-    out[C] = regs[A]
-    return out
-
-@store
-def seti(A, B, C, regs):
-    out = regs.copy()
-    out[C] = A
-    return out
-
-@store
-def gtir(A, B, C, regs):
-    out = regs.copy()
-    out[C] = 1 if A > regs[B] else 0
-    return out
-
-@store
-def gtri(A, B, C, regs):
-    out = regs.copy()
-    out[C] = 1 if regs[A] > B else 0
-    return out
-
-@store
-def gtrr(A, B, C, regs):
-    out = regs.copy()
-    out[C] = 1 if regs[A] > regs[B] else 0
-    return out
-
-@store
-def eqir(A, B, C, regs):
-    out = regs.copy()
-    out[C] = 1 if A == regs[B] else 0
-    return out
-
-@store
-def eqri(A, B, C, regs):
-    out = regs.copy()
-    out[C] = 1 if regs[A] == B else 0
-    return out
-
-@store
-def eqrr(A, B, C, regs):
-    out = regs.copy()
-    out[C] = 1 if regs[A] == regs[B] else 0
-    return out
-
+def cmp_maker():
+    """Generates instructions that correspond to comparisons"""
+    instrs = {}
+    # choose operation type
+    ops = {'gt': gt, 'eq': eq}
+    for name,op in ops.items():
+        for tail_A,tail_B in 'ri','rr','ir':
+            def fun(A, B, C, regs, op=op, tail_A=tail_A, tail_B=tail_B):
+                out = regs.copy()
+                out[C] = 1 if op(A if tail_A == 'i' else regs[A], B if tail_B == 'i' else regs[B]) else 0
+                return out
+            instrs[name + tail_A + tail_B] = fun
+    return instrs
 
 def day16(inp, part2=False):
     itinp = iter(inp.split('\n'))
-    hits = 0
+
+    # create instruction table
+    instrs = {**binary_maker(), **asgn_maker(), **cmp_maker()}
+
     examples = []
     for line in itinp:
         if line.startswith('Before'):
@@ -123,23 +59,18 @@ def day16(inp, part2=False):
             # end of part 1
             if not part2:
                 break # needed for test case
+
             next(itinp)
             program = [list(map(int, cmd.split())) for cmd in itinp if cmd.strip()]
             break
 
         # we have from_reg, the instruction and to_reg
         # see which functions match this example
-        matchset = set()
-        for name,fun in instrs.items():
-            if to_reg == fun(A, B, C, from_reg):
-                matchset.add(name)
-
+        matchset = {name for name,fun in instrs.items() if fun(A, B, C, from_reg) == to_reg}
         examples.append((ind, matchset)) # for each example in a list we have (instruction index, matching names) tuples
 
-        # part 1: get counts
-        if len(examples[-1][1]) >= 3:
-            # part 1
-            hits += 1
+    # part 1: get counts
+    hits = sum(1 for example in examples if len(example[1]) >= 3)
 
     if not part2:
         return hits
@@ -148,17 +79,11 @@ def day16(inp, part2=False):
     known = {}
     while len(known) < 16:
         new_knowns = {ind:next(iter(names)) for ind,names in examples if len(names) == 1}
-        if not new_knowns:
-            print(f'Oh-uh!')
-            print(f'known = {known}')
-            print(f'examples = {examples}')
-            print({ind:len(names) for ind,names in examples})
-            break
+        assert new_knowns # if this happens we're screwed
         new_names = set(new_knowns.values())
         known.update(new_knowns)
         examples = [example for example in examples if len(example[1]) > 1]
         for ind,candidates in examples:
-            len0 = len(candidates)
             candidates -= new_names
 
     regs = [0]*4
@@ -168,14 +93,8 @@ def day16(inp, part2=False):
     return hits,regs[0]
 
 
-
 if __name__ == "__main__":
     testinp = open('day16.testinp').read()
-    #testinp2 = open('day16.testinp2').read()
     inp = open('day16.inp').read()
     print(f'part1 test: {day16(testinp)}')
-    #print(f'part1 test: {testinp2} -> {day16(testinp2)}')
-    #print(f'part1: {day16(inp)}')
-    #print(f'part2 test: {testinp} -> {day16b(testinp)}')
-    #print(f'part2 test: {testinp2} -> {day16b(testinp2)}')
     print(f'part2: {day16(inp, part2=True)}')
