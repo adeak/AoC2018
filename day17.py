b296d5e54fd4515ea8f813639b90a2a41ca05cc3
adeak <adeak@users.noreply.github.com>
2018-12-17 23:43:52 +0100

    Day 17 improved

--- day17.py
+++ day17.py
@@ -1,15 +1,15 @@
 from itertools import count
-from collections import deque
 import numpy as np
-from scipy.ndimage.morphology import binary_dilation
 
 def print_map(view):
     printview = np.empty_like(view.T, dtype='U1')
-    for char,val in zip('.#|~', count()):
+    for val,char in enumerate('.#|~'):
         printview[view.T == val] = char
     print('\n'.join([''.join(row) for row in printview]))
 
 def day17(inp):
+    DRY,CLAY,WET,STILL = range(4)
+
     dat = []
     xmin = ymin = np.inf
     xmax = ymax = -np.inf
@@ -31,85 +31,91 @@ def day17(inp):
             xmin = min(xmin, fromto[0])
             xmax = max(xmax, fromto[1])
 
-    # add one to the size along x in each direction in order to have clayless walls
+    # add one to the size along x in each direction in order to have clayless walls on the side
     xmin -= 1
     size = (xmax - xmin + 1 + 2, ymax - ymin + 1)
     view = np.zeros(size, dtype=int) # zero is sand, 1 is clay, 2 is running water, 3 is still water
     for dir,ind,fromto in dat:
         if dir == 'x':
-            view[ind - xmin, fromto[0] - ymin:fromto[1]+1 - ymin] = 1
+            view[ind-xmin, fromto[0]-ymin:fromto[1]+1-ymin] = CLAY
         else:
-            view[fromto[0] - xmin:fromto[1] + 1 - xmin, ind - ymin] = 1
+            view[fromto[0]-xmin:fromto[1]+1-xmin, ind-ymin] = CLAY
     # spring is at view[500 - xmin, 0 - ymin] except these coordinates are outside the array!
 
-    flowers = [(500 - xmin, max(0, 0 - ymin))] # first pixel where water flows; proper indices from now on
-    # flow|er, not flower
-    prev_waters = deque([0,0,0], maxlen=3) # number of "wet" pixels in three rounds
+    # keep track of pixels that actively propagate the flow
+    flowers = {(500 - xmin, max(0, 0 - ymin))} # first pixel where water flows; proper indices from now on
+    # (flow|er, not flower)
+    prev_water = 0
     while True:
-        # set current flow edge to running water
         #print_map(view)
         #input()
-        for i,flower in list(enumerate(flowers))[::-1]:
-            view[flower] = 2
+        for x0,y0 in flowers.copy():
+            # move "down" if possible, include "this" pixel
+            for y in count(y0):
+                # wet this pixel
+                view[x0, y] = WET
+                # move the corresponding edge pixel
+                flowers.add((x0, y))
+                flowers -= {(x0, y - 1)}
 
-            # move "down" if possible
-            x0,y0 = flower
-            #invalidate = False
-            if y0 == size[1] - 1:
-                # there's nowhere to flow "down"
-                continue
-            if view[x0, y0 + 1] == 0:
-                # sand; we can flow there, add edge
-                view[x0, y0 + 1] = 2
-                flowers.append((x0, y0 + 1))
-                #invalidate = True
-
-            # move to the side if possible
-            if view[x0, y0 + 1] in [1, 3]:
-                # clay or still water; we need to start spreading out
-                nextpos = (x0 - 1, y0)
-                if 0 <= nextpos[0] and view[nextpos] == 0 and nextpos not in flowers:
-                    # spread there
-                    flowers.append(nextpos)
-                    #invalidate = True
-                nextpos = (x0 + 1, y0)
-                if nextpos[0] < size[0] and view[nextpos] == 0 and nextpos not in flowers:
-                    # spread there
-                    flowers.append(nextpos)
-                    #invalidate = True
-                #if invalidate:
-                #    # we need to restart the loop over flow-ends
-                #    break
+                if y == size[1] - 1 or view[x0, y + 1] == WET:
+                    # we reached the bottom or an existing flow, stop flowing
+                    break
+                if view[x0, y + 1] in {CLAY, STILL}:
+                    # we can't keep flowing down, start flowing horizontally
+                    for x in range(x0 - 1, -1, -1):
+                        # flow to the left
+                        if view[x, y] == CLAY:
+                            # we hit a wall
+                            break
+                        view[x, y] = WET
+                        flowers.add((x, y))
+                        flowers -= {(x + 1, y)}
+                        if view[x, y + 1] not in {CLAY, STILL}:
+                            # we have to stop flowing in this direction
+                            break
+                    for x in range(x0 + 1, size[1]):
+                        # flow to the right
+                        if view[x, y] == CLAY:
+                            # we hit a wall
+                            break
+                        view[x, y] = WET
+                        flowers.add((x, y))
+                        flowers -= {(x - 1, y)}
+                        if view[x, y + 1] not in {CLAY, STILL}:
+                            # we have to stop flowing in this direction
+                            break
+                    break
 
         # check for filled cavern rows
-        left_walls = (view[:-1,:] == 1) & (view[1:,:] == 2)
+        left_walls = (view[:-1,:] == CLAY) & (view[1:,:] == WET)
         for iwall, jwall in zip(*left_walls.nonzero()):
-            water_inds = []
+            water_inds = set()
             for iwater in count(iwall+1):
-                if iwater > size[0] - 1:
-                    # assume that we can't have still water here due to overflows outside...
+                assert iwater <= size[0] - 1 # opposite should never happen, edge should always be free
+
+                if jwall + 1 < size[1] and view[iwater, jwall + 1] == DRY:
+                    # then we can flow down here, no still water
                     break
-                if jwall + 1 < size[1] and view[iwater, jwall + 1] == 0:
-                    # then we can still flow down, no still water
+                # the next position can only be sand, running water or a wall; sand means we can't be still
+                if view[iwater, jwall] == DRY:
+                    # we're flowing to the right
                     break
-                # the next position can only be sand, running water or a wall
-                if view[iwater, jwall] == 0:
-                    # we're flowing to the right, no still water here
-                    break
-                if view[iwater, jwall] == 2:
-                    water_inds.append((iwater,jwall))
-                if view[iwater, jwall] == 1:
-                    # we've reached the other wall, got still water here
-                    view[iwall + 1:iwater, jwall] = 3
-                    flowers = [flower for flower in flowers if flower not in water_inds]
+                if view[iwater, jwall] == WET:
+                    water_inds.add((iwater,jwall))
+                if view[iwater, jwall] == CLAY:
+                    # we've reached the other wall, means still water here
+                    view[iwall + 1:iwater, jwall] = STILL
+                    # update edges with flows from previous row
+                    new_flowers = {(iwall + 1 + iedge, jwall - 1) for iedge in (view[iwall + 1:iwater, jwall - 1] == WET).nonzero()[0]}
+                    flowers = (flowers | new_flowers) - water_inds
                     break
 
-        num_water = np.isin(view, [2,3]).sum()
-        prev_waters.append(num_water)
-        if len(set(prev_waters)) == 1:
+        num_water = np.isin(view, [WET, STILL]).sum()
+        if num_water == prev_water:
             # we're stationary
-            #print_map(view)
-            return num_water, (view == 3).sum()
+            return num_water, (view == STILL).sum()
+        prev_water = num_water
 
 
 if __name__ == "__main__":
@@ -117,8 +123,3 @@ if __name__ == "__main__":
     inp = open('day17.inp').read()
     print(f'part1-2 test: {day17(testinp)}')
     print(f'part1-2: {day17(inp)}')
-    # 35613 too low :(
-    # add 13 due to capillaries
-    # 35626 too low
-    # 35630 too low
-    # 35682 "not the right answer"
