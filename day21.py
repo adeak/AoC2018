ce2667ed2cdee545da1b3044f0dc54357f477601
adeak <adeak@users.noreply.github.com>
2018-12-21 17:17:44 +0100

    Day 21: add slow but generic solution

--- day21.py
+++ day21.py
@@ -1,6 +1,47 @@
 from operator import itemgetter
+from day16 import instruction_maker
 
-def day21_manual(inp):
+def day21_generic(inp):
+    """Generic solution, very slow (runs in ~45 minutes)"""
+    instrs = instruction_maker()
+
+    header,*rest = inp.strip().split('\n')
+    ip = int(header.split()[1])
+    program = [[row.split()[0],*map(int,row.split()[1:])] for row in rest]
+    Ni = len(program)
+
+    # try to guess the key register, assuming register 0 only gets compared to this once
+    pre_exit,cmp_reg = next((i,({*program[i][1:3]} - {0}).pop()) for i in range(Ni)
+                            if program[i][0] == 'eqrr' and 0 in {*program[i][1:3]})
+
+    regs = [0]*6
+    ni = 0
+    halters = {}
+    seens = set()
+    while 0 <= regs[ip] < Ni:
+        ni += 1
+        if regs[ip] == pre_exit:
+            # we'd be comparing regs[0] and regs[cmp_reg] now
+            val = regs[cmp_reg]
+            if val not in halters:
+                # this value for regs[0] would halt in ni instructions
+                halters[val] = ni
+            else:
+                if tuple(regs) in seens:
+                    # we're just going to cycle from here; stop
+                    return (min(halters.items(), key=itemgetter(1))[0],
+                            max(halters.items(), key=itemgetter(1))[0])
+            seens.add(tuple(regs))
+        name,A,B,C = program[regs[ip]]
+        regs = instrs[name](A, B, C, regs)
+        regs[ip] += 1
+    if ip == 0:
+        # jump outside without writing it back into the register
+        regs[ip] -= 1
+
+
+def day21_manual():
+    """Specific solution, slow (runs in ~1.5 minutes)"""
     r1 = r2 = r3 = r4 = r5 = 0
 
     # ignore dead code: same number of instructions for each case
@@ -51,4 +92,5 @@ def day21_manual(inp):
 
 if __name__ == "__main__":
     inp = open('day21.inp').read()
-    print(f'part1-2: {day21_manual(inp)}')
+    #print(f'part1-2: {day21_generic(inp)}')
+    print(f'part1-2: {day21_manual()}')
